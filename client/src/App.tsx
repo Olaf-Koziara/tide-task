c97006b045e7281ea17bbc8510ec444465a04b8b
Olaf1303 <olaf.koziara@gmail.com>
2025-12-18 17:59:37 +0100

    Integrate CityList component into App for enhanced city display alongside CitySearch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 import { CitySearch } from './components/CitySearch/CitySearch'
+import { CityList } from './components/CityList/CityList'
 
 function App() {
   return (
     <main className="app">
       <CitySearch />
+      <CityList />
     </main>
   )
 }
