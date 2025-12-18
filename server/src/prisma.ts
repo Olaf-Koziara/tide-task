3a403032fce8063d02384570b8d1bb28a6921f4a
Olaf1303 <olaf.koziara@gmail.com>
2025-12-18 12:49:45 +0100

    Integrate Prisma ORM with SQLite support, update package dependencies, and configure environment variables. Add Prisma schema and client initialization.

---
+++ server/src/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client";
+import { env } from "./config/env";
+
+export const prisma = new PrismaClient({
+    datasources: { db: { url: env.DATABASE_URL } }
+});
+
