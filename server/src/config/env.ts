dbd11f214738940906297b4054e2ff7f996e2d50
Olaf1303 <olaf.koziara@gmail.com>
2025-12-18 12:50:43 +0100

    Update environment configuration to change default PORT to 4000 and add DATABASE_URL for Prisma database connection.

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -2,7 +2,8 @@ import 'dotenv/config';
 import z from 'zod';
 
 const EnvSchema = z.object({
-    PORT: z.coerce.number().int().positive().default(3000),
- 
-})
-export const env =  EnvSchema.parse(process.env);  
+    PORT: z.coerce.number().int().positive().default(4000),
+    DATABASE_URL: z.string().trim().default('file:./prisma/dev.db')
+});
+
+export const env = EnvSchema.parse(process.env);
